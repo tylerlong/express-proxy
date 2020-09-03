5cad1fedcd4dca7f65813bb8e53444f2f3bff93b
Tyler Long <tyler4long@gmail.com>
2020-09-03 14:52:29 -0700

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import proxy from 'express-http-proxy';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use('/microsoft', proxy('https://login.microsoftonline.com'));
 
 const port = 3000;
