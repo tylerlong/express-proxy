d20fa2d750a6675be352cc9a9023fdd841161fab
Tyler Long <tyler4long@gmail.com>
2020-09-03 15:17:19 -0700

    First fully working version

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import express from 'express';
 import proxy from 'express-http-proxy';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 app.use(cors());
-app.use('/microsoft', proxy('https://login.microsoftonline.com'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(
+  '/microsoft',
+  proxy('https://login.microsoftonline.com', {
+    proxyReqBodyDecorator: function (bodyContent) {
+      if (bodyContent.client_secret === undefined) {
+        bodyContent.client_secret = process.env.MICROSOFT_CLIENT_SECRET;
+      }
+      return bodyContent;
+    },
+  })
+);
 
 const port = 3000;
 app.listen(port, () => {
